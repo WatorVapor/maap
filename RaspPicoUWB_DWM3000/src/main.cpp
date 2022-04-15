5ac4afa34d8e7fe3c35045afc10992456d882d85
Wator Vapor <watorvapor@gmail.com>
2022-04-15 23:10:13 +0900

    Update main.cpp

--- RaspPicoUWB_DWM3000/src/main.cpp
+++ RaspPicoUWB_DWM3000/src/main.cpp
@@ -1,17 +1,13 @@
 #include <Arduino.h>
-#include "pico/multicore.h"
 
 void uwb_setup(void);
 void uwb_one_loop(void);
-void c(void);
 void setup() {
   Serial.begin(115200);
-  Serial.print("core0:start....\r\n");
   uwb_setup();
 }
 
 void loop() {
-  Serial.print("core0:start....\r\n");
   uwb_one_loop();
   delay(100);
 }
