765f093fe1fdd1761d8af3f67b848319dfd4e60b
Wator Vapor <watorvapor@gmail.com>
2022-04-15 22:33:29 +0900

    Update uwb.cpp

--- RaspPicoUWB_DWM3000/src/uwb.cpp
+++ RaspPicoUWB_DWM3000/src/uwb.cpp
@@ -3,8 +3,8 @@ extern "C" {
     #include "deca_device_api.h"
     #include "deca_regs.h"
     #include "deca_spi.h"
-    #include "shared_defines.h"
 }
+#include "shared_defines.h"
 #include "port.h"
 #include "debug.hpp"
 
@@ -26,6 +26,9 @@ static dwt_config_t config = {
 
 extern dwt_txconfig_t txconfig_options;
 
+int simple_tx_setup(void);
+int simple_tx_loop(void);
+
 void uwb_setup (void) {
     peripherals_init();
     spi_peripheral_init();
@@ -45,8 +48,7 @@ void uwb_setup (void) {
         ERROR_VAR(ret2);
     }
     dwt_configuretxrf(&txconfig_options);
-    dwt_setlnapamode(DWT_LNA_ENABLE | DWT_PA_ENABLE);
-    dwt_configuresleep(DWT_CONFIG, DWT_PRES_SLEEP | DWT_WAKE_CSN | DWT_WAKE_WUP | DWT_SLP_EN);
+    simple_tx_setup();
 }
 
 
@@ -55,7 +57,17 @@ static const int32_t BLINK_FRAME_SN_IDX  = 1;
 static const int32_t  FRAME_LENGTH   =  sizeof(tx_msg)+FCS_LEN;
 static const int32_t TX_DELAY_MS = 1000;
 
+
 void uwb_one_loop(void) {
+    simple_tx_loop();
+}
+
+
+int simple_tx_setup(void) {
+    dwt_setlnapamode(DWT_LNA_ENABLE | DWT_PA_ENABLE);
+    dwt_configuresleep(DWT_CONFIG, DWT_PRES_SLEEP | DWT_WAKE_CSN | DWT_WAKE_WUP | DWT_SLP_EN);
+}
+int simple_tx_loop(void) {
     dwt_writetxdata(FRAME_LENGTH-FCS_LEN, tx_msg, 0);
     dwt_writetxfctrl(FRAME_LENGTH, 0, 0);
     dwt_starttx(DWT_START_TX_IMMEDIATE);
