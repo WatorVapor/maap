921e514e027af261a21d7b55f2ec71dd96b8d6d0
Wator Vapor <watorvapor@gmail.com>
2022-06-08 14:56:28 +0900

    Add files via upload

---
+++ static-www/www/board/bu01/index.js
@@ -0,0 +1,8 @@
+const appPrefix = '/maap';
+const loadFrameWorkJS = ()=> {
+  const fileref = document.createElement('script');
+  fileref.setAttribute('type','text/javascript');
+  fileref.setAttribute('src', `${appPrefix}/assets/js/app.js`);
+  document.getElementsByTagName('head')[0].appendChild(fileref);
+}
+loadFrameWorkJS();
